8d3f331bf47aa1f89586f2a24c6fcb0b2afa424d
5pierre <plouis.lieske@gmail.com>
2025-10-17 15:26:48 +0200

    update

---
+++ src/Controller/BookController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+
+
+
+
+
+//CRUD
+
+
+// ajouter un livre
+
+// modifier un livre
+
+// supprimer un livre
+
+// recuperer les livre
+
---
+++ src/Controller/EmpruntController.php
@@ -0,0 +1,8 @@
+
+<!-- Ajoutez une route permettant de faire une demande d’emprunt de livre 
+Ajoutez une route permettant de rendre un livre 
+Ajoutez une route permettant de savoir pour un utilisateur combien d’emprunts il a en 
+cours (l’affichage sera trié par date de la plus ancienne à la plus récente) 
+
+// Ajoutez une route pour récupérer tous les livres d’un auteur emprunté entre 2 dates 
+// définies  -->
